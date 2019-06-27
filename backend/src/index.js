c1967e568c9f792866f187d69b9d121a316653de
Tommi Hietanen <tthietanen@gmail.com>
2019-06-27 12:08:27 +0300

    APPID harcoding removed, pass it in container eg. by env

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || 'e2004025b16ddecf48c7a3adb0208100';
+const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
