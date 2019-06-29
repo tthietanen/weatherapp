15ad078613b7526c58afa258cc01bc97fb5870eb
Tommi Hietanen <tthietanen@gmail.com>
2019-06-29 13:33:48 +0300

    output changed

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,15 +9,16 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    contentBase: 'src/public',
+    contentBase: './dist',
     historyApiFallback: true,
     port: 8000,
     host: '0.0.0.0',
   },
   devtool: 'eval',
   output: {
-    filename: 'index.jsx',
+    path: __dirname + '/dist',
     publicPath: '/',
+    filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -40,7 +41,7 @@ module.exports = {
     ], '.'),
     new webpack.DefinePlugin({
       'process.env': {
-        ENDPOINT: JSON.stringify(process.env.ENDPOINT || 'http://0.0.0.0:9000/api'),
+        ENDPOINT: JSON.stringify(process.env.ENDPOINT || 'http://localhost:9000/api'),
       },
     }),
   ],
