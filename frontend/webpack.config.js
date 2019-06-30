3e85297df0b7d5d5d034eaecf62e213d6fbbbd48
Tommi Hietanen <tthietanen@gmail.com>
2019-06-30 11:44:01 +0300

    frontend dockerized

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        ENDPOINT: JSON.stringify(process.env.ENDPOINT || 'http://localhost:9000/api'),
+        ENDPOINT: JSON.stringify(process.env.ENDPOINT || 'http://0.0.0.0:9000/api'),
       },
     }),
   ],
