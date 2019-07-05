49e629aee8f36f408c5f96577ed325bbfcfdda66
Tommi Hietanen <tthietanen@gmail.com>
2019-07-05 09:04:26 +0300

    frontend showing icon (text) & dt & dt_txt

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -15,36 +15,49 @@ const getWeatherFromApi = async () => {
   return {};
 };
 
-class Forecast extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      forecast: [],
+      forecasts: [],
     };
   }
 
   async componentWillMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({forecast: weather});
+    const result = await getWeatherFromApi();
+    this.setState({forecasts: result});
   }
 
   render() {
     return (
-
-	    <div>
-	    <ul>
-	    {this.state.forecast.map(item => (
-		    <li key={JSON.stringify(item.dt_txt)}>{JSON.stringify(item)}</li>
+      <div>
+	<ul>
+	    {this.state.forecasts.map( item => (
+		    <li key={JSON.stringify(item.dt_txt)}>
+		    	<Forecast forecast={item} />
+		    </li>
 	    ))}
-	    </ul>
-	    </div>
+        </ul>
+      </div>
 
     );
   }
 }
 
+const Forecast = ({ forecast }) => (
+	<div>
+	    <Icon icon={forecast.weather[0].icon} />
+	    <Dt dt={forecast.dt} />
+	    <Dt_txt dt_txt={forecast.dt_txt} />
+	</div>
+);
+
+const Icon = ({ icon }) => <h3>{icon}</h3>;
+const Dt = ({ dt }) => <h3>{dt}</h3>;
+const Dt_txt = ({ dt_txt }) => <h3>{dt_txt}</h3>;
+
 ReactDOM.render(
-  <Forecast />,
+  <App />,
   document.getElementById('app')
 );
